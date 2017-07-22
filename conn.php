32e3237b6ce22e16e7b2a38ebdafa795c9d8ed5c
peerfahad <fahadrashid611@gmail.com>
2017-07-22 11:20:16 -0700

    Modified files

--- conn.php
+++ conn.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "handworker";
+$dbname = "handwork";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
